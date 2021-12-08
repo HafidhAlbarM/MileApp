a04669917d03bb700cca0070d21d04b9fb8e1e61
Hafidh <hafidh2704@gmail.com>
2021-12-08 19:42:36 +0700

    ignore node_module

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require("./app");
 require("dotenv").config();
 
 app.listen(process.env.PORT, () => {
-  console.log(`berjalan di port ${process.env.PORT}`);
+  console.log(`berjalan di portt ${process.env.PORT}`);
 });
