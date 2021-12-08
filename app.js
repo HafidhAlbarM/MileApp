2ff134e756792b4d7ae7c76cba05cdc4b460ae3e
Hafidh <hafidh2704@gmail.com>
2021-12-08 19:48:17 +0700

    done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const swaggerUI = require("swagger-ui-express");
-const apiDocumentation = require("./apidocs.json");
+const apiDocumentation = require("./documentation/apidocs.json");
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(apiDocumentation));
 
 const apiRoutes = require("./src/routes/api");
