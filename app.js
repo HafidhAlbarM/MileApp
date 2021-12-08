c61d983bb11a5d72789483130bd989b89efb1c7a
Hafidh <hafidh2704@gmail.com>
2021-12-08 09:34:01 +0700

    Finish Unit Test

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-require("dotenv").config();
-require("./config/database");
+require("./src/config/database");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const apiRoutes = require("./routes/api");
+const apiRoutes = require("./src/routes/api");
 
 app.use("/api", apiRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`berjalan di port ${process.env.PORT}`);
-});
+module.exports = app;
